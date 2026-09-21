a2c3a2a13e1803642f02b5164dfd65cb7486c1f7
Hami Ibriyamov <hamop3108@gmail.com>
2026-09-21 11:53:54 +0200

    Create IntToString.java

---
+++ Java/IntToString.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        String s = Integer.toString(n);
+        
+        // Check if the conversion was successfull
+        if (s.getClass().getName() == "java.lang.String") {
+            System.out.println("Good job");
+        } else {
+            System.out.println("Wrong answer");  
+        }
+    }
+}
